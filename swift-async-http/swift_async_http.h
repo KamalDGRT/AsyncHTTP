d591a65361cec117c70563be39c481b0e62597b2
KamalDGRT <kamaldgrt@gmail.com>
2025-02-23 13:11:24 +0530

    Initial Commit

---
+++ swift-async-http/swift_async_http.h
@@ -0,0 +1,18 @@
+//
+//  swift_async_http.h
+//  swift-async-http
+//
+//  Created by Kamal Sharma on 23/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift_async_http.
+FOUNDATION_EXPORT double swift_async_httpVersionNumber;
+
+//! Project version string for swift_async_http.
+FOUNDATION_EXPORT const unsigned char swift_async_httpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_async_http/PublicHeader.h>
+
+
